37b9a65a1b29bd6e6a1d9a24dcaae8574f7a66b2
alex emery <alex.emery@couchbase.com>
2023-04-04 16:29:50 +0100

    added ID to buffer line for faster jumping

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -3,6 +3,10 @@ return {
   version = "v3.*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require("bufferline").setup()
+    require("bufferline").setup {
+      options = {
+        numbers = "buffer_id",
+      }
+    }
   end,
 }
